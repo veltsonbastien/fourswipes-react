429986b87460c98e40b2f63e9a887aadd3947697
veltsonbastien <bastienveltson@outlook.com>
2021-02-25 10:51:16 -0800

    added in more components, and learned more about how we'll handle certain CRUD things

---
+++ components/footer.jsx
@@ -0,0 +1,13 @@
+//The props is where we would be able to pass in info
+//such as title, data, author, and perchance the actual
+//article, which might just be it's own Component as well.
+import styles from '../styles/Home.module.css'
+
+
+export default function Footer(props){
+  return(
+    <div>
+     <h3>Semantic Footer Text copyright 2021</h3> 
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Pane from '../components/pane.jsx'
+import Footer from '../components/footer.jsx'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <div className = {styles.maindesc}> Here's what's happening: </div>
       <div className = {styles.downarrow}> v </div>
     </div>
+    {/*Four "Featured Panes"*/}
     <Pane
      style = {{backgroundColor: '#2083bf'}}
      panetitle = "Some interesting news."
@@ -19,6 +21,28 @@ export default function Home() {
      panedesc = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
      paneimgsrc = "https://compote.slate.com/images/222e0b84-f164-4fb1-90e7-d20bc27acd8c.jpg?width=1920"
      />
+     <Pane
+      style = {{backgroundColor: '#27ae60'}}
+      panetitle = "Some interesting news."
+      panetag = "Interesting"
+      panedesc = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      paneimgsrc = "https://compote.slate.com/images/222e0b84-f164-4fb1-90e7-d20bc27acd8c.jpg?width=1920"
+     />
+     <Pane
+       style = {{backgroundColor: '#e74c3c'}}
+       panetitle = "Some interesting news."
+       panetag = "Interesting"
+       panedesc = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+       paneimgsrc = "https://compote.slate.com/images/222e0b84-f164-4fb1-90e7-d20bc27acd8c.jpg?width=1920"
+      />
+      <Pane
+        style = {{backgroundColor: '#bdc3c7'}}
+        panetitle = "Some interesting news."
+        panetag = "Interesting"
+        panedesc = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        paneimgsrc = "https://compote.slate.com/images/222e0b84-f164-4fb1-90e7-d20bc27acd8c.jpg?width=1920"
+       />
+     <Footer/>
   </>
   )
 }
